6b73caac0f1ea0c80bba964205810436715aabf7
yushi <yushiwh@126.com>
2019-06-19 14:05:32 +0800

    增加shiro+jwt+MQ的支持，上传文件的方法支持 nick

--- sql/test.sql
+++ sql/test.sql
@@ -12,26 +12,6 @@ CREATE TABLE `test_user`
 
 
 -- nick 20190617
-CREATE TABLE `version`
-(
-    `id`                BIGINT(20)   NOT NULL COMMENT '主键',
-    `app_name`          VARCHAR(50)  NOT NULL DEFAULT '' COMMENT '更新后启动的程序',
-    `application_start` VARCHAR(50)  NOT NULL DEFAULT '' COMMENT '程序名称',
-    `force_update`      BIGINT(20)   NOT NULL DEFAULT '0' COMMENT '是否强制更新 1-是  0-否',
-    `min_version`       VARCHAR(50)  NOT NULL DEFAULT '' COMMENT '最小版本号',
-    `release_date`      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
-    `release_url`       VARCHAR(200) NOT NULL DEFAULT '' COMMENT '下载地址',
-    `release_version`   VARCHAR(50)  NOT NULL DEFAULT '' COMMENT '发布版本号',
-    `update_mode`       VARCHAR(50)  NOT NULL DEFAULT 'Increment' COMMENT 'Cover表示覆盖更新，Increment表示增量更新',
-    `version_desc`      VARCHAR(50)  NOT NULL DEFAULT '' COMMENT '更新界面提示信息',
-    `create_at`         TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`         TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    PRIMARY KEY (`id`)
-)
-    COMMENT ='自动升级表'
-    COLLATE = 'utf8_general_ci'
-    ENGINE = InnoDB
-;
 
 CREATE TABLE `user`
 (
@@ -52,20 +32,6 @@ CREATE TABLE `user`
     ENGINE = InnoDB
 ;
 
-
-CREATE TABLE `user_config`
-(
-    `id`            BIGINT(20) NOT NULL DEFAULT '0' COMMENT '编号',
-    `user_agent_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT 'user表中userAgentId',
-    `config_json`   TEXT       NOT NULL COMMENT '配置json信息',
-    `creat_at`      TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`     TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    PRIMARY KEY (`id`)
-)
-    COMMENT ='用户配置信息表，针对的是小工具的配置'
-    COLLATE = 'utf8_general_ci'
-    ENGINE = InnoDB
-;
 
 
 -- *****************************  shiro  开始  不用上线 ***************************************
