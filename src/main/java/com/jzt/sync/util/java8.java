174cc0d9d8dc0a020d31c37a54448d8fa046b2fb
nick <94149094@qq.com>
2020-07-29 11:16:06 +0800

    增加java8的

---
+++ src/main/java/com/jzt/sync/util/java8.java
@@ -0,0 +1,31 @@
+package com.jzt.sync.util;
+
+
+import org.apache.ibatis.javassist.runtime.Desc;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+@Component
+public class java8 {
+    public void getData() {
+        List<String> list = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+        Stream<String> stream = list.stream();
+        stream.forEach(System.out::println);
+
+        System.out.println("*********************************");
+
+        List<Integer> listInt = Arrays.asList(2, 1, 4, 5, 3);
+        Stream<Integer> streamInt = listInt.stream();
+
+        streamInt.map(i -> i * i * i).filter(i -> i >= 4).limit(4).sorted().forEach(System.out::println);
+        System.out.println("*********************************");
+
+        Random random = new Random();
+        random.ints(1, 100).limit(10).forEach(System.out::println);
+
+    }
+}
